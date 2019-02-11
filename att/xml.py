c228d744a1a947e8f77e1d6760f70b32abe84d3c
nandinicm <nandini.cm@razorthink.com>
2019-02-11 13:23:28 +0530

    Xml write

---
+++ att/xml.py
@@ -0,0 +1,76 @@
+import re
+
+res = {
+  "all_Fields": [
+    {
+      "id": "1",
+      "tag": "InvoiceNumber",
+      "type": "Key-value pair",
+      "key": "invoice number",
+      "value": "361685",
+      "children": [
+        {
+          "id": "2",
+          "tag": "total_current_charges",
+          "type": "Key-value pair",
+          "key": "invoice number",
+          "value": "361685",
+          "children": []
+        },
+        {
+          "id": "3",
+          "tag": "AccountNumber",
+          "type": "Key-value pair",
+          "key": "account number",
+          "value": "112227",
+          "children": []
+        }
+      ]
+    }
+  ],
+  "page_Data": [
+    {
+      "pageNumber": 0,
+      "confidenceScore": 0
+    },
+    {
+      "pageNumber": -1,
+      "confidenceScore": 1
+    }
+  ]
+}
+invoice_info_list = ['total_current_charges', 'late_charges', 'PDB']
+fields = res["all_Fields"]
+header = '<invoice '
+invoice_infos = []
+invoice_details = []
+for f in fields:
+    header += f['tag'] + '="' + f['value'] + '" '
+    for f1 in f['children']:
+        child_tag = '<charge '
+        if f1['tag'] in invoice_info_list:
+            child_tag += 'amount="' + f1['value'] + '" type="' + f1['tag'] + '"/>'
+            invoice_infos.append(child_tag)
+        else:
+            child_tag += 'amount="' + f1['value'] + '" description="' + f1['tag'] + \
+                         '" eng_description="' + f1['tag'] + '"/>'
+            invoice_details.append(child_tag)
+header = re.sub(' $', '>', header)
+
+xml_output = header
+if invoice_infos:
+    xml_output += '<invoice_info>'
+    for info in invoice_infos:
+        xml_output += info
+        xml_output += '</invoice_info>'
+if invoice_details:
+    xml_output += '<invoice_details>'
+    xml_output += '<line item="">'
+    for detail in invoice_details:
+        xml_output += detail
+    xml_output += '</line>'
+    xml_output += '</invoice_details>'
+xml_output += '</invoice>'
+print(xml_output)
+with open('/home/rztuser/test.xml', "w") as fs:
+    fs.write(xml_output)
