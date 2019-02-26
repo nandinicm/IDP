b7003372b028eb31c3076e4b8921b46fdb19b47a
nandinicm <nandini.cm@razorthink.com>
2019-02-26 11:46:39 +0530

    Coordintes

--- att/soln_script.py
+++ att/soln_script.py
@@ -118,9 +118,11 @@ def get_page_hypothesis(image, evidence, rules):
 
     structures = s_engine.run(image, word_args=(word_patches_dict,))
     all_results, tb = c_engine.run([image], [structures])
+    # print("Traceback------------------------------------------", tb)
+    # with open('/home/devops/logs.txt', 'a') as f:
+    # f.write(str(tb))
+    # f.write('--------------------------------------------------------------------------')
     return (format_data(all_results, tb, rules, evidence["pageNumber"]))
-
-
 
     # for page, img in enumerate(imgs, 1):
     #     try:
@@ -139,6 +141,8 @@ def get_page_hypothesis(image, evidence, rules):
     # # # exit()
     #
     # print(format_data(all_results,tb,rules,evidence["pageNumber"]))
+
+
 def is_inside(rect1, rect2):  # (x1, y1, x1b, y1b), (x2, y2, x2b, y2b)):
     # if check_data():
     left = rect2[3] < rect1[1]  # always -ve
@@ -146,39 +150,48 @@ def is_inside(rect1, rect2):  # (x1, y1, x1b, y1b), (x2, y2, x2b, y2b)):
     bottom = rect2[0] > rect1[2]  # always +ve
     top = rect2[2] < rect1[0]  # always -ve
     if top or left or right or bottom:
+        # print('IS INSIDE IS FALSE')
         return False
     else:  # rectangles intersect
+        # print('IS INSIDE IS TRUE')
         return True
 
 
 def mark_positions(image):
-    position_clause={
-    'top-half':[0,0,int(image.shape[1]/2),image.shape[0]],
-    'bottom-half':[int(image.shape[1]/2),0,int(image.shape[1]),image.shape[0]],
-    'left-half':[0,0,int(image.shape[1]),int(image.shape[0]/2)],
-    'right-half':[0,int(image.shape[0]/2),int(image.shape[1]),int(image.shape[0])],
-    'top-left-corner':[0,0,int(image.shape[1]/2),int(image.shape[0]/2)],
-    'top-right-corner':[0,int(image.shape[0]/2),int(image.shape[1]/2),int(image.shape[0])],
-    'bottom-left-corner':[int(image.shape[1]/2),0,int(image.shape[1]),int(image.shape[0]/2)],
-    'bottom-right-corner':[int(image.shape[1]/2),int(image.shape[0]/2),int(image.shape[1]),int(image.shape[0])],
-    'all':[0,0,image.shape[1],image.shape[0]]
+    position_clause = {
+        'top-half': [0, 0, int(image.shape[0] / 2), image.shape[1]],
+        'bottom-half': [int(image.shape[0] / 2), 0, int(image.shape[0]), image.shape[1]],
+        'left-half': [0, 0, int(image.shape[0]), int(image.shape[1] / 2)],
+        'right-half': [0, int(image.shape[1] / 2), int(image.shape[0]), int(image.shape[1])],
+        'top-left-corner': [0, 0, int(image.shape[0] / 2), int(image.shape[1] / 2)],
+        'top-right-corner': [0, int(image.shape[1] / 2), int(image.shape[0] / 2), int(image.shape[1])],
+        'bottom-left-corner': [int(image.shape[0] / 2), 0, int(image.shape[0]), int(image.shape[1] / 2)],
+        'bottom-right-corner': [int(image.shape[0] / 2), int(image.shape[1] / 2), int(image.shape[0]),
+                                int(image.shape[1])],
+        'all': [0, 0, image.shape[0], image.shape[1]]
     }
     return position_clause
 
-def filter_by_coordinates(field_list,rules,position_clause):
-    poped_enum=[]
-    print("ALLRULES",rules)
-    for field_enum,each_field in enumerate(field_list):
-        inside_coordinates_flag=False
-        #print("FIELDS ::",each_field)
-        #print("EACH_RULES :",each_field['tag'])
-        #print(rules[each_field['tag']]['hints']['where'])
+
+def filter_by_coordinates(field_list, rules, position_clause):
+    poped_enum = []
+    print("ALL RULES", rules)
+    for field_enum, each_field in enumerate(field_list):
+        inside_coordinates_flag = False
+        # print('####'*35)
+        # print("FIELDS ::",each_field)
+        # print("EACH_RULES :",each_field['tag'])
+        # print(rules[each_field['tag']]['hints']['where'])
         # print("ALL",position_clause['all'])
+        # count=0
         for each_pos in rules[each_field['tag']]['hints']['where']['position']['values']:
+            # count=count+1
+            # print("count ::",count)
             if is_inside(position_clause[each_pos], [each_field['coord']['y'], each_field['coord']['x'],
                                                      (each_field['coord']['y'] + each_field['coord']['height']),
                                                      (each_field['coord']['x'] + each_field['coord']['width'])]):
-                inside_coordinates_flag=True
+                # print('Passed')
+                inside_coordinates_flag = True
                 break
         if not inside_coordinates_flag:
             poped_enum.append(field_enum)
@@ -186,19 +199,19 @@ def filter_by_coordinates(field_list,rules,position_clause):
     # print('POPED ENUM',poped_enum)
     for enum in poped_enum:
         field_list.pop(enum)
-    #print("FIELD_LIST ARE ",field_list)
+    # print("FIELD_LIST ARE ",field_list)
     return field_list
 
 
-def filter_on_where_rules(im,page_results,rules):
+def filter_on_where_rules(im, page_results, rules):
     position_clause = mark_positions(im)
-    page_results['fields']=filter_by_coordinates(page_results['fields'],rules,position_clause)
+    page_results['fields'] = filter_by_coordinates(page_results['fields'], rules, position_clause)
     return page_results
 
 
 if __name__ == "__main__":
     input_list = sys.argv
-    image_folder=input_list[1] + "images/"
+    image_folder = input_list[1] + "images/"
     # image_folder = input_list[1]
     evidence_folder = input_list[1] + "words/"
     output_folder = input_list[1] + "fields/"
@@ -209,25 +222,25 @@ if __name__ == "__main__":
 
     # with open(rule_json) as fp:
     #     str_ = fp.readline()
-    #print("RULE HERE:",rule_json)
+    # print("RULE HERE:",rule_json)
     rules = json.load(open(rule_json))
-    #print(rules)
-    rules2=deepcopy(rules)
+    # print(rules)
+    rules2 = deepcopy(rules)
 
-    for k,v in rules.items():
-        v['hints']['where']={}
+    for k, v in rules.items():
+        v['hints']['where'] = {}
     # rules[]['where']={}
-    #print("RULE1 :",rules)
-    #print("RULE2 :",rules2)
+    # print("RULE1 :",rules)
+    # print("RULE2 :",rules2)
 
     # print(rules.keys())
     # exit()
 
     # doc_level_hypothesis=[]
-    page_count=1
+    page_count = 1
     for evidence_name in os.listdir(evidence_folder):
-        print("PAGE COUNT :",page_count)
-        page_count=page_count+1
+        print("PAGE COUNT :", page_count)
+        page_count = page_count + 1
         if evidence_name.startswith("."):
             continue
 
@@ -236,18 +249,22 @@ if __name__ == "__main__":
             str_evidence = fp.readline()
 
         evidence = ast.literal_eval(str_evidence)
-        #print("EVIDENCES :",evidence)
+        # print("EVIDENCES :",evidence)
         print("img: " + image_folder + evidence_name[:-4].replace("_page", "") + "jpg")
         im = cv2.imread(image_folder + evidence_name[:-4].replace("_page", "") + "jpg")
-        #print("startloading")
-        #print(type(im))
-        #print("image loaded")
+        # print("startloading")
+        # print(type(im))
+        # print("image loaded")
 
         # page_results = {"pageNumber": 0, "fields": [{"id": 0, "tag": "Account Number", "confidenceScore": 1.0, "coord": {"x": 838, "y": 237, "width": 74, "height": 26}, "type": "Key-value pair", "value": "{'key': 'account no', 'value': '112227'}"}], "confidenceScore": 1.0}
         page_results = get_page_hypothesis(image=im, evidence=evidence, rules=rules)
         print(page_results)
         print('---------------------------------------------------------------------------------')
         page_results = filter_on_where_rules(im, page_results, rules2)
+
+        print("AFTER WHERE RULES")
+        print(page_results)
+        print('*' * 100)
         json.dump(page_results, open(output_folder + (evidence_name), "w"))
         # doc_level_hypothesis.append(page_results)
         # print(json.load(open(evidence_folder+image_name[:-3] + "JSON")))
@@ -255,5 +272,3 @@ if __name__ == "__main__":
         # print("****************************************")
         # print(doc_level_hypothesis)
         # print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
-
-
